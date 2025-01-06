d74e0280fe7ac14746dec241e943e4e274ae1af8
pmc0707 <pranshugoodboy@gmail.com>
2025-01-06 23:51:08 +0530

    implementation of home page

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -2,9 +2,34 @@ import React from 'react';
 
 function Awards() {
     return (  
-        <h1>
-            Awards
-        </h1>
+       <div className='container mt-5'>
+        <div className='row'>
+        <div className='row col-6'>
+            <img src='/media/images/largestBroker.svg' />
+        </div>
+        <div className='col-6'>
+            <h1>Largest stock broker in India</h1>
+            <p>That's why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
+            <div className='row'>
+                <div className='col-6'>
+                <ul>
+                
+                <li><p>Future and Options</p></li>
+                <li><p>Commodity derivatives</p></li>
+                <li><p>Currency derivatives</p></li>
+            </ul>
+                </div>
+                <div className='col-6'> <ul>
+                
+                <li><p>Future and Options</p></li>
+                <li><p>Commodity derivatives</p></li>
+                <li><p>Currency derivatives</p></li>
+                
+            </ul></div>
+            </div>                
+        </div>
+        </div>
+       </div>
     );
 }
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,9 +1,14 @@
 import React from 'react';
 function Hero() {
     return ( 
-        <h1>
-            Hero <i class="fa fa-user-circle" aria-hidden="true"></i>
-        </h1>
+        <div className='container p-5 mb-10'>
+            <div className='row text-center'>
+                <img src='media/images/homeHero.png' className='mb-5' />
+                <h1 className='mt-5'>Invest in everthing</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button className='p-2 btn btn-primary fs-5' style={{width:"17%", margin: "0 auto"}}>Signup for free Now</button>
+            </div>
+        </div>
      );
 }
 
