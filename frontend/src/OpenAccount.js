aaf4f4967af4630ae492bcead2c3e43176677943
pmc0707 <pranshugoodboy@gmail.com>
2025-01-10 00:02:56 +0530

    update route

--- frontend/src/OpenAccount.js
+++ frontend/src/OpenAccount.js
@@ -4,7 +4,7 @@ function OpenAccount() {
     return ( 
         <div className='container p-5 mb-10'>
             <div className='row text-center'>
-                <img src='media/images/homeHero.png' className='mb-5' />
+                
                 <h1 className='mt-5'>Open Zerodha account</h1>
                 <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
                 <button className='p-2 btn btn-primary fs-5' style={{width:"17%", margin: "0 auto"}}>Sign up Now</button>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './landing_page/home/HomePage'; 
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Signup from './landing_page/signup/Signup';
+import AboutPage from './landing_page/about/AboutPage';
+import ProductPage from './landing_page/product/ProductPage';
+import Pricing from './landing_page/home/Pricing';
+import SupportPage from './landing_page/support/SupportPage'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  
-    <HomePage />
-
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<HomePage/>}/>
+    <Route path='/signup' element={<Signup/>}/>
+    <Route path='/about' element={<AboutPage/>}/>
+    <Route path='/product' element={<ProductPage/>}/>
+    <Route path='/pricing' element={<Pricing/>}/>
+    <Route path='/support' element={<SupportPage/>}/>
+  </Routes>
+  </BrowserRouter>
 );
