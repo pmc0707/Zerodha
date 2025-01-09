54c9303609f92a117ac1a103aa0998ec8c1ee375
pmc0707 <pranshugoodboy@gmail.com>
2025-01-09 21:57:04 +0530

    resturcture of openaccount

--- frontend/src/OpenAccount.js
+++ frontend/src/OpenAccount.js
@@ -5,8 +5,8 @@ function OpenAccount() {
         <div className='container p-5 mb-10'>
             <div className='row text-center'>
                 <img src='media/images/homeHero.png' className='mb-5' />
-                <h1 className='mt-5'>Invest in everthing</h1>
-                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <h1 className='mt-5'>Open Zerodha account</h1>
+                <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
                 <button className='p-2 btn btn-primary fs-5' style={{width:"17%", margin: "0 auto"}}>Signup for free Now</button>
             </div>
         </div>
