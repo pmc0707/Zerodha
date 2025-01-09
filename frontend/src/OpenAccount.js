96bdd15cdc745089a2ce4c645a71582e04702daf
pmc0707 <pranshugoodboy@gmail.com>
2025-01-09 21:58:00 +0530

    rename signup

--- frontend/src/OpenAccount.js
+++ frontend/src/OpenAccount.js
@@ -7,7 +7,7 @@ function OpenAccount() {
                 <img src='media/images/homeHero.png' className='mb-5' />
                 <h1 className='mt-5'>Open Zerodha account</h1>
                 <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-                <button className='p-2 btn btn-primary fs-5' style={{width:"17%", margin: "0 auto"}}>Signup for free Now</button>
+                <button className='p-2 btn btn-primary fs-5' style={{width:"17%", margin: "0 auto"}}>Sign up Now</button>
             </div>
         </div>
      );
