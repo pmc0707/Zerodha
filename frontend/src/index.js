2e8ca59cef1d709da7d610913474b32946b25938
pmc0707 <pranshugoodboy@gmail.com>
2025-01-11 00:09:15 +0530

    implementation of pricing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Signup from './landing_page/signup/Signup';
 import AboutPage from './landing_page/about/AboutPage';
 import ProductPage from './landing_page/product/ProductPage';
-import Pricing from './landing_page/home/Pricing';
+import PricingPage from './landing_page/pricing/PricingPage';
 import SupportPage from './landing_page/support/SupportPage';
 import Topnav from './Topnav';
 import Footer from './Footer';
@@ -20,7 +20,7 @@ root.render(
     <Route path='/signup' element={<Signup/>}/>
     <Route path='/about' element={<AboutPage/>}/>
     <Route path='/product' element={<ProductPage/>}/>
-    <Route path='/pricing' element={<Pricing/>}/>
+    <Route path='/pricing' element={<PricingPage/>}/>
     <Route path='/support' element={<SupportPage/>}/>
     <Route path='*' element={<NotFound/>}/>
   </Routes>
