fbc0e51746b1620196fa0cdf87a893e05b1fe667
pmc0707 <pranshugoodboy@gmail.com>
2025-01-21 00:14:45 +0530

    home page route change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import HomePage from './landing_page/home/HomePage.js'; // Correct path to HomePage
+import HomePage from './landing_page/home/HomePage' // Correct path to HomePage
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './landing_page/signup/Signup';
 import AboutPage from './landing_page/about/AboutPage';
