7a8db1673459dc43c37565d64c74d94e11d8e1be
pmc0707 <pranshugoodboy@gmail.com>
2025-01-20 20:53:20 +0530

    turn off dependency warning

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ["react-app"],
+    plugins: ["jsx-a11y"],
+    rules: {
+      "jsx-a11y/anchor-is-valid": "off",
+      // Add other rules if needed
+    },
+  };
+  
