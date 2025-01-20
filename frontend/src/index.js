8fa7549b7ef25f83c5b98776cbb093096bfa4266
pmc0707 <pranshugoodboy@gmail.com>
2025-01-21 00:00:40 +0530

    update index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import HomePage from './landing_page/home/HomePage.js'; 
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from './landing_page/home/HomePage.js'; // Correct path to HomePage
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './landing_page/signup/Signup';
 import AboutPage from './landing_page/about/AboutPage';
 import ProductPage from './landing_page/product/ProductPage';
@@ -12,20 +12,21 @@ import Topnav from './Topnav';
 import Footer from './Footer';
 import NotFound from './NotFound';
 import Login from './landing_page/login/Login';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <Topnav/>
-  <Routes>
-    <Route path='/' element={<HomePage/>}/>
-    <Route path='/signup' element={<Signup/>}/>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/about' element={<AboutPage/>}/>
-    <Route path='/product' element={<ProductPage/>}/>
-    <Route path='/pricing' element={<PricingPage/>}/>
-    <Route path='/support' element={<SupportPage/>}/>
-    <Route path='*' element={<NotFound/>}/>
-  </Routes>
-  <Footer/>
+    <Topnav />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/product' element={<ProductPage />} />
+      <Route path='/pricing' element={<PricingPage />} />
+      <Route path='/support' element={<SupportPage />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+    <Footer />
   </BrowserRouter>
 );
