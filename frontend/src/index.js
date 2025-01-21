f7db1b52035fadf8327a165bd180f99a03a1a06e
pmc0707 <pranshugoodboy@gmail.com>
2025-01-21 11:20:37 +0530

    chNge the dierction

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import HomePage from './landing_page/home/HomePage' // Correct path to HomePage
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './landing_page/home/HomePage'; // Correct path to HomePage
 import Signup from './landing_page/signup/Signup';
 import AboutPage from './landing_page/about/AboutPage';
 import ProductPage from './landing_page/product/ProductPage';
