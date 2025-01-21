3e63127bbd68b0ddefaa7b9e6835dd7e3eb7920e
pmc0707 <pranshugoodboy@gmail.com>
2025-01-21 11:15:05 +0530

    upadte packeage

--- frontend/src/landing_page/home/Homepage.js
+++ frontend/src/landing_page/home/Homepage.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import Hero from './Hero';
-import Awards from './Awards';
-import Education from './Education';
-import Pricing from './Pricing';
-import Stats from './Stats';
-import OpenAccount from '../../OpenAccount';
+    import React from 'react';
+    import Hero from './Hero';
+    import Awards from './Awards';
+    import Education from './Education';
+    import Pricing from './Pricing';
+    import Stats from './Stats';
+    import OpenAccount from '../../OpenAccount';
 
 
-function HomePage() {
-    return ( 
-       <>
-           
-           <Hero />
-           <Awards />
-           <Stats />
-           <Pricing />
-           <Education />
-           <OpenAccount />
-           
-       </>
-    );
-}
+    function HomePage() {
+        return ( 
+        <>
+            
+            <Hero />
+            <Awards />
+            <Stats />
+            <Pricing />
+            <Education />
+            <OpenAccount />
+            
+        </>
+        );
+    }
 
-export default HomePage;
+    export default HomePage;
