918a9ee05222afe116ee44a9f4f820df99faed5e
pmc0707 <pranshugoodboy@gmail.com>
2025-01-21 11:29:39 +0530

    changes in homepage

--- frontend/src/landing_page/home/Homepage.js
+++ frontend/src/landing_page/home/Homepage.js
@@ -1,25 +1,27 @@
-    import React from 'react';
-    import Hero from './Hero';
-    import Awards from './Awards';
-    import Education from './Education';
-    import Pricing from './Pricing';
-    import Stats from './Stats';
-    import OpenAccount from '../../OpenAccount';
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
 
+import OpenAccount from "../OpenAccount";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
-    function HomePage() {
-        return ( 
-        <>
-            
-            <Hero />
-            <Awards />
-            <Stats />
-            <Pricing />
-            <Education />
-            <OpenAccount />
-            
-        </>
-        );
-    }
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+      <Footer />
+    </>
+  );
+}
 
-    export default HomePage;
+export default HomePage;
