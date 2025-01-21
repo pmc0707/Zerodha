701a4877db7aa4826b316f33b44bdcec39f33532
pmc0707 <pranshugoodboy@gmail.com>
2025-01-21 11:48:10 +0530

    update route

--- frontend/src/landing_page/home/Homepage.js
+++ frontend/src/landing_page/home/Homepage.js
@@ -1,28 +1,3 @@
-    // import React from 'react';
-    // import Hero from './Hero';
-    // import Awards from './Awards';
-    // import Education from './Education';
-    // import Pricing from './Pricing';
-    // import Stats from './Stats';
-    // import OpenAccount from '../../OpenAccount';
-
-
-    // function HomePage() {
-    //     return ( 
-    //     <>
-            
-    //         <Hero />
-    //         <Awards />
-    //         <Stats />
-    //         <Pricing />
-    //         <Education />
-    //         <OpenAccount />
-            
-    //     </>
-    //     );
-    // }
-
-    // export default HomePage;
 import React from "react";
 import Hero from "./Hero";
 import Awards from "./Awards";
